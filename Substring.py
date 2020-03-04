307d2fa92297dcd1a4f7e22a1387569c3c0c78ea
Vikash kumar <36899219+vikashyap99@users.noreply.github.com>
2020-03-04 19:46:08 +0530

    Add files via upload

---
+++ Substring.py
@@ -0,0 +1,42 @@
+def max_length(li,maxx):
+	xx=0
+	for i in range(26):
+		if(li[i]):
+			xx+=1
+	return (xx==maxx)
+
+
+s = input() 
+n = len(s)
+ls = [0]*40
+for i in range(n):
+	ls[ord(s[i])-ord('a')]=1
+maxx=0
+ans = 1
+for i in range(26):
+	maxx += ls[i]
+low=1
+high=n
+while(low<=high):
+	mid=(low+high)//2
+	f=[0]*30
+	x=0
+	for i in range(mid):
+		f[ord(s[i])-ord('a')] += 1
+	for j in range(n-mid):
+		if(max_length(f,maxx)):
+			x=1
+			break
+
+		f[ord(s[j-1])-ord('a')]-=1
+		f[ord(s[j+mid-1])-ord('a')]+=1
+
+	if(max_length(f,maxx)):
+		x=1
+	if(x):
+		ans=mid
+		high=mid-1
+	else:
+		low=mid+1
+
+print(maxx)
